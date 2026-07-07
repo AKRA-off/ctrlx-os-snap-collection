4161bef4b98479a410f49518e6f91727ad5ca3b0
AKRA-off <60703770+AKRA-off@users.noreply.github.com>
2026-07-07 09:46:55 +0700

    Update setup.py

--- moveit-mtc_kassow_onrobot/src/joystick_control/setup.py
+++ moveit-mtc_kassow_onrobot/src/joystick_control/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'joystick_teleop = joystick_control.joystick_teleop:main',
+            'joystick_teleop = joystick_control.joystick_teleop_node:main',
             'start_servo = joystick_control.start_servo:main',
         ],
     },
